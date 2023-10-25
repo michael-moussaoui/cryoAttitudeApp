e31bd33b8a52bee3eec6e748862a45315846060e
lankeycode <michael.moussaoui@gmail.com>
2023-10-25 13:30:50 +0200

    Backend: Add server

---
+++ Backend/models/index.js
@@ -0,0 +1,20 @@
+const config = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+	config.DB,
+	config.USER,
+	config.PASSWORD,
+	{
+		host: config.HOST,
+		dialect: config.dialect,
+		pool: {
+			max: config.pool.max,
+			min: config.pool.min,
+			acquire: config.pool.acquire,
+			idle: config.pool.idle,
+		},
+	}
+);
+
+const db = {};
---
+++ Backend/server.js
@@ -0,0 +1,47 @@
+if (typeof PhusionPassenger !== "undefined") {
+	PhusionPassenger.configure({ autoInstall: false });
+}
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+	origin: [
+		process.env.LOCAL_CLIENT,
+		process.env.REMOTE_CLIENT_APP,
+		process.env.LOCAL_CLIENT_VITE_APP,
+		process.env.REMOTE_SERVER_API,
+		process.env.LOCAL_CLIENT_MOBILE,
+		process.env.LOCAL_CLIENT_MOBILE2,
+	],
+	methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+	allowedHeaders:
+		"Origin, X-Requested-With, x-access-token, role, Content, Accept, Content-Type, Authorization",
+	credentials: true,
+	exposedHeaders: ["Content-Range"],
+	optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route for test
+app.get("/", (req, res) => {
+	res.json({ message: "Welcome to Attitude Cryo." });
+});
+
+const db = require("./models");
+
+if (typeof PhusionPassenger !== "undefined") {
+	app.listen("passenger");
+} else {
+	app.listen(process.env.SERVER_PORT);
+}
